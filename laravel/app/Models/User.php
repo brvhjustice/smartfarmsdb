1be4b973a15d1b9ac6ffceb4be78a77e4ff5af8c
Brvh Justice <brvhjustice@gmail.com>
2022-11-10 01:19:52 +0000

    added fName to user in model folder

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -18,7 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'f_name',
+        // 'l_name',
+        'phone',
         'email',
         'password',
     ];
