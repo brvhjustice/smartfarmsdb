0b3a14e5f7a4fe7c66076d61a94943206abdad76
Brvh Justice <brvhjustice@gmail.com>
2022-11-19 04:03:49 +0000

    s

--- laravel/app/Admin/routes.php
+++ laravel/app/Admin/routes.php
@@ -14,7 +14,7 @@ Route::group([
     $router->get('/', 'HomeController@index')->name('home');
    
     $router->resource('products', ProductController::class);
-    $router->resource('product-types', ProductTypeController::class); 
+    // $router->resource('product-types', ProductTypeController::class); 
     $router->resource('/users', UserController::class);
   
 
